c6244b10d4fb2f4218433b4e2be141b33024c862
rokealvo <rokealvo@gmail.com>
2019-10-26 22:38:19 +0700

    add slider

--- src/main.js
+++ src/main.js
@@ -1,15 +1,29 @@
+import './../node_modules/swiper/css/swiper.min.css'
 import './main.scss';
 import './blocks/feedback-form/feedback-form'
+import Swiper from 'swiper';
 
-(function main(){
+(function main() {
   const burger = document.querySelector('.header__burger');
   const nav = document.querySelector('.header__nav')
 
-  if (screen.width<=1200) {
+  if (screen.width <= 1200) {
     nav.classList.toggle('hidden')
   }
 
-  burger.addEventListener('click', (e)=>{
+  burger.addEventListener('click', (e) => {
     nav.classList.toggle('hidden')
   })
+
+  const mySwiper = new Swiper('.swiper-container', {
+
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+  });
 })()
