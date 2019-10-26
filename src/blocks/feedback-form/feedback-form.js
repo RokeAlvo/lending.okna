c28af7f109f4124d1077098b6155e2793964c5ea
rokealvo <rokealvo@gmail.com>
2019-10-26 18:30:41 +0700

    build docs

--- src/blocks/feedback-form/feedback-form.js
+++ src/blocks/feedback-form/feedback-form.js
@@ -1,3 +1,14 @@
 import './feedback-form.scss'
 import './../input-text/input-text'
 import './../button/button'
+
+(function (){
+  const forms = document.querySelectorAll('.feedback-form');
+  Array.from(forms).forEach(form=>{
+    form.addEventListener('submit', e=>{
+      e.preventDefault()
+      const input = e.target.querySelector('input')
+      console.log('телефон', input.value)
+    })
+  })
+})()
