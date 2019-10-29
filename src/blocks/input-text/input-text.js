c8f94d6cdab54ca1dcfd65246b9fb74d68df3246
rokealvo <rokealvo@gmail.com>
2019-10-29 23:28:31 +0700

    add inputmask

--- src/blocks/input-text/input-text.js
+++ src/blocks/input-text/input-text.js
@@ -1,1 +1,11 @@
 import './input-text.scss'
+
+import Inputmask from "inputmask";
+// import "inputmask/lib/extensions/inputmask.numeric.extensions";
+
+(function (){
+  const selectors = document.querySelectorAll('input[type=tel]');
+  Array.from(selectors).forEach(selector=>{
+    Inputmask({"mask": "+7(999) 999-99-99"}).mask(selector);
+  })
+})()
